924024686525847d35999ad760b0d7694e9c3ee3
Robert Zettl <zettl.robert@gmail.com>
2020-01-07 17:33:26 +0100

    Added Response Object

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to the Contact Keeper API' })
+})
+
+
 const PORT = process.env.PORT || 5000;
 
 
