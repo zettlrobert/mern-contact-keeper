8eb3a0707bfa620ad2b7578dd0f5196d4a6f77a7
Robert Zettl <zettl.robert@gmail.com>
2020-01-07 20:49:58 +0100

    Create and Respond with a JWT

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config')
 const {
   check,
   validationResult
 } = require('express-validator');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -47,7 +49,20 @@ router.post('/', [
 
     await user.save();
 
-    res.send('User saved');
+    // Response after Registration
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(payload, config.get('jwtSecret'), {
+      expiresIn: 36000
+    }, (err, token) => {
+      if (err) throw err;
+      res.json({ token })
+    }
+    );
 
   } catch (err) {
     console.error(err.message);
