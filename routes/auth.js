83cefaf5a2ca5aadb83d5ab9edc73e12e759ad94
Robert Zettl <zettl.robert@gmail.com>
2020-01-08 14:50:59 +0100

    Auth Middleware &$ Protecting Private Routes Get loged In User

--- routes/auth.js
+++ routes/auth.js
@@ -8,13 +8,23 @@ const {
   check,
   validationResult
 } = require('express-validator');
+const auth = require('../middleware/auth');
 
 
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-router.get('/', (req, res) => {
-  res.send(`Get logged in user`)
+router.get('/', auth, async (req, res) => {
+  try {
+    // Get user from Database
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+
+  }
 });
 
 
